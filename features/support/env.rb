a75e397f553e789bc3b17c3bbb25f6c752f7a5d5
goraxe <goraxe@goraxe.me.uk>
2012-06-18 21:05:24 -0700

    remove references to cobbler

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,7 @@
 #require the relevant libraries
 require 'rubygems'
 require 'libvirt'
-require 'xmlrpc/client'
+require 'xmlsimple'
 require 'net/ssh'
 require 'ping'
 require 'socket'
@@ -15,9 +15,6 @@ require 'yaml'
 Before do
     # load in the YAML config
     config_file = YAML::load(File.open('config.yml'))
-    #cobbler server:
-    @cobbler_server = config_file['cobbler']['server']
-    @cobbler_port = config_file['cobbler']['port']
 
     #Libvirt
     @libvirt_driver = config_file['libvirt']['driver']
@@ -29,11 +26,11 @@ Before do
     ## VM Connection
     @vmconn =  Libvirt::open("#{@libvirt_driver}://#{@libvirt_host}/#{@libvirt_type}")
     ### VM Storage
-    @vm_stor = @vmconn.lookup_storage_pool_by_name(@libvirt_storage_pool)   
+    # @vm_stor = @vmconn.lookup_storage_pool_by_name(@libvirt_storage_pool)   
 
 
     ## Cobbler API
-    @cblr_api = XMLRPC::Client.new(@cobbler_server,"/cobbler_api",@cobbler_port)
+    # @cblr_api = XMLRPC::Client.new(@cobbler_server,"/cobbler_api",@cobbler_port)
 
     ### define the port-checker code
     def is_port_open(ip, port)
